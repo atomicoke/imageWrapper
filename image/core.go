7b2e9d3564c7741e074e44ea7970f12c0f578b51
weiZhen <likelovec@gmail.com>
2022-05-04 11:33:02 +0800

    ref #NewWrap

--- image/core.go
+++ image/core.go
@@ -1,8 +1,49 @@
 package image
 
 import (
+	"bytes"
+	"errors"
+	"github.com/disintegration/imaging"
+	"io"
+	"io/ioutil"
 	"net/http"
 )
+
+func NewWrap(body io.ReadCloser, width, height int) (*Wrap, error) {
+	defer body.Close()
+
+	bs, err := ioutil.ReadAll(body)
+	if err != nil {
+		return nil, errors.New("Failed to read image\ncause:" + err.Error())
+	}
+
+	// decode bytes to image
+	img, err := Decode(bs)
+	if err != nil {
+		return nil, errors.New("Failed to decode image\ncause:" + err.Error())
+	}
+
+	// get image contentType and extension
+	contentType := http.DetectContentType(bs)
+	format, err := parseType(contentType)
+	if err != nil {
+		return nil, errors.New("Failed to parse image type\ncause:" + err.Error())
+	}
+
+	// save the resized image
+	data := bytes.NewBuffer([]byte{})
+
+	// resize image
+	img = imaging.Resize(img, width, height, imaging.Lanczos)
+
+	err = imaging.Encode(data, img, format)
+	if err != nil {
+		return nil, errors.New("Failed to encode image\ncause:" + err.Error())
+	}
+
+	return createImage(data, format, contentType).
+		PutHeader("Content-Type", contentType), nil
+}
 
 func (w *Wrap) PutHeader(key, val string) *Wrap {
 	w.header.Add(key, val)
--- image/factory.go
+++ image/factory.go
@@ -2,48 +2,9 @@ package image
 
 import (
 	"bytes"
-	"errors"
 	"github.com/disintegration/imaging"
-	_ "image/gif"
-	_ "image/jpeg"
-	_ "image/png"
-	"io"
-	"io/ioutil"
 	"net/http"
 )
-
-func NewWrap(body io.ReadCloser, width, height int) (*Wrap, error) {
-	defer body.Close()
-
-	bs, err := ioutil.ReadAll(body)
-	if err != nil {
-		return nil, errors.New("Failed to read image\ncause:" + err.Error())
-	}
-
-	//img, name, err := image.Decode(bytes.NewReader(bs))
-	img, err := Decode(bs)
-	if err != nil {
-		return nil, errors.New("Failed to decode image\ncause:" + err.Error())
-	}
-
-	img = imaging.Resize(img, width, height, imaging.Lanczos)
-	data := bytes.NewBuffer([]byte{})
-
-	contentType := http.DetectContentType(bs)
-
-	format, err := parseType(contentType)
-	if err != nil {
-		return nil, errors.New("Failed to parse image type\ncause:" + err.Error())
-	}
-
-	err = imaging.Encode(data, img, format)
-	if err != nil {
-		return nil, errors.New("Failed to encode image\ncause:" + err.Error())
-	}
-
-	return createImage(data, format, contentType).
-		PutHeader("Content-Type", contentType), nil
-}
 
 func BuildKey(resizeStr string, imageUrl string) string {
 	return resizeStr + " - " + imageUrl
